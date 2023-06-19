2d4297ce6628fd5e0b828ba49c8f57c2c9035f99
Jason Lee <zkla201@naver.com>
2023-06-20 02:50:26 +0900

    test

---
+++ Casting.java
@@ -0,0 +1,15 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		double a = 2.4;
+		double b = 1;
+		
+		int c = (int) 1;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
